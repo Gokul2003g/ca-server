9bd1383abce2a4306e505ebde1ab8908dbea0dc8
nitin-gr <nitin.gr@icloud.com>
2024-08-21 17:12:21 +0530

    Get access token from frontend
    Signed-off-by: nitin-gr <nitin.gr@icloud.com>

--- src/routes.rs
+++ src/routes.rs
@@ -1,18 +1,23 @@
 use crate::key_signer::sign_key;
-use crate::models::SignRequest;
+use crate::models::{BearerToken, SignRequest};
 use rocket::http::Status;
 use rocket::serde::json::Json;
 
-#[get("/")]
-pub fn index() -> String {
-    String::from("Hello World")
-}
-
 #[post("/handle-post", data = "<data>")]
-pub fn handle_post(data: Json<SignRequest>) -> String {
-    match sign_key(data.public_key.as_str(), data.is_host) {
-        Ok(cert) => cert,
-        Err(err) => err.to_string(),
+pub fn handle_post(token: Result<BearerToken, Status>, data: Json<SignRequest>) -> String {
+    match token {
+        Ok(token) => {
+            println!("Extracted Token: {}", token.0); // Logging the extracted token
+
+            match sign_key(data.public_key.as_str(), data.is_host) {
+                Ok(cert) => cert,
+                Err(err) => err.to_string(),
+            }
+        }
+        Err(status) => {
+            // If token extraction failed, return the corresponding status code
+            status.to_string()
+        }
     }
 }
 
